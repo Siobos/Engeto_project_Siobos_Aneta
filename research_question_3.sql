1c8d486ad5fc2e36d2a14844670e9dd07a8a6f0e
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-04 15:15:31 +0100

    Update research_question_3.sql

--- research_question_3.sql
+++ research_question_3.sql
@@ -1,1 +1,26 @@
+/* RQ 3) Which food category is increasing in price the slowest (has the lowest percentage year-on-year increase)? */
 
+WITH czechia_price_selection_calculation AS (
+SELECT *,
+	LEAD(avg_value, 1) OVER (
+	PARTITION BY category_code
+ORDER BY year
+	) lead_function
+FROM czechia_price_selection
+)
+SELECT *, ROUND((lead_function-avg_value)/(avg_value)*100,0) AS YOY_change
+FROM czechia_price_selection_calculation;
+
+/* Better detail: */
+
+WITH czechia_price_selection_calculation AS (
+SELECT *,
+	LEAD(avg_value, 1) OVER (
+	PARTITION BY category_code
+ORDER BY year
+	) lead_function
+FROM czechia_price_selection
+)
+SELECT category_code, name, unit, ROUND(AVG(lead_function-avg_value)/(avg_value)*100,2) AS AVG_YOY_change
+FROM czechia_price_selection_calculation
+GROUP BY category_code;
