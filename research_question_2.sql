352b6a488ac7c96c67b24061384b2b4a33ca0492
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-10 13:35:32 +0100

    Update research_question_2.sql

--- research_question_2.sql
+++ research_question_2.sql
@@ -10,3 +10,15 @@ SELECT YEAR, avg_value, unit
 FROM t_aneta_siobos_project_sql_primary_final taspspf  
 WHERE YEAR IN (2006, 2018) AND category_code IN ('111301','114201')
 GROUP BY YEAR, category_code;
+
+/* More accurate results can be obtained by finding average wages directly from the input data (Czechia_payroll), 
+where there are data for which the sector was not specified more closely. The required primary table (t_aneta_siobos_project_sql_primary_final) 
+has been purged of this data. */
+
+CREATE TABLE average_payroll_2006_2018
+SELECT 
+payroll_year,
+round(avg(value),2) AS average_payroll_per_year
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 200 AND payroll_year IN (2006,2018)
+GROUP BY payroll_year;
