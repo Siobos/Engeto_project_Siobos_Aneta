91ebdb7ef0c6d642978c39d2f46bf232ecc4df15
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-10 13:37:33 +0100

    Update research_question_2.sql

--- research_question_2.sql
+++ research_question_2.sql
@@ -1,15 +1,19 @@
 /* RQ 2) How many liters of milk and kilograms of bread can be bought in the first and last comparable periods in the available price and wage data? */
 
-SELECT YEAR, 
+SELECT 
+	YEAR, 
 	ROUND(avg(avg_value),2) AS avg_value, unit
-FROM t_aneta_siobos_project_sql_primary_final taspspf  
-WHERE YEAR IN (2006, 2018) AND category_code IN ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S')
+	FROM t_aneta_siobos_project_sql_primary_final taspspf  
+WHERE YEAR IN (2006, 2018) AND unit = 'Kč' 
 GROUP BY YEAR
 UNION
-SELECT YEAR, avg_value, unit
+SELECT 
+	YEAR, 
+	avg_value, 
+	unit
 FROM t_aneta_siobos_project_sql_primary_final taspspf  
-WHERE YEAR IN (2006, 2018) AND category_code IN ('111301','114201')
-GROUP BY YEAR, category_code;
+WHERE YEAR IN (2006, 2018) AND code IN ('111301','114201')
+GROUP BY YEAR, code;
 
 /* More accurate results can be obtained by finding average wages directly from the input data (Czechia_payroll), 
 where there are data for which the sector was not specified more closely. The required primary table (t_aneta_siobos_project_sql_primary_final) 
@@ -17,8 +21,8 @@ has been purged of this data. */
 
 CREATE TABLE average_payroll_2006_2018
 SELECT 
-payroll_year,
-round(avg(value),2) AS average_payroll_per_year
+	payroll_year,
+	round(avg(value),2) AS average_payroll_per_year
 FROM czechia_payroll
 WHERE value_type_code = 5958 AND calculation_code = 200 AND payroll_year IN (2006,2018)
 GROUP BY payroll_year;
