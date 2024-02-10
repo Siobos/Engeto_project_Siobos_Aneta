4f00f6c4697a597db98ddc0786d1ec16a7327ee8
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-10 14:34:25 +0100

    Update research_question_3.sql

--- research_question_3.sql
+++ research_question_3.sql
@@ -8,6 +8,20 @@ ORDER BY year
 	) lead_function
 FROM czechia_price_selection
 )
+SELECT *, ROUND((lead_function-avg_value)/(avg_value)*100,2) AS YOY_change
+FROM czechia_price_selection_calculation
+GROUP BY code, YEAR;
+
+--Better detail--
+
+WITH czechia_price_selection_calculation AS (
+SELECT *,
+	LEAD(avg_value, 1) OVER (
+	PARTITION BY code
+ORDER BY year
+	) lead_function
+FROM czechia_price_selection
+)
 SELECT 
 	code, 
 	name, 
