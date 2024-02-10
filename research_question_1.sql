b6a7e4fda3ebec014a25e60147b49d0f910d5353
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-10 11:38:00 +0100

    Update research_question_1.sql

--- research_question_1.sql
+++ research_question_1.sql
@@ -5,7 +5,7 @@ RQ 1) Are wages rising in all industries over the years, or falling in some? */
 WITH t_aneta_siobos_project_sql_primary_final_calculations AS (
 SELECT *,
 	LEAD(avg_value, 1) OVER (
-	PARTITION BY category_code
+	PARTITION BY code
 ORDER BY year
 	) results
 FROM t_aneta_siobos_project_sql_primary_final taspspf
@@ -14,9 +14,8 @@ SELECT *,
 	CASE
         WHEN results > avg_value THEN 'increase'
         WHEN results < avg_value THEN 'decrease'
-        WHEN results = avg_value THEN 'stagnation'
         ELSE '-'
     END AS grow
 FROM t_aneta_siobos_project_sql_primary_final_calculations
-WHERE category_code IN ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S') AND YEAR BETWEEN 2011 AND 2020
-ORDER BY grow, category_code, year;
+WHERE unit = 'Kč' AND YEAR BETWEEN 2011 AND 2020
+ORDER BY grow, code, YEAR;
