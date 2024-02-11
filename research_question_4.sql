6f20ca8891510af1760c836ba76ac1b8c53d16ab
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-11 08:18:53 +0100

    Update research_question_4.sql

--- research_question_4.sql
+++ research_question_4.sql
@@ -2,13 +2,23 @@
 
 WITH czechia_price_selection_YOY_change AS (
     SELECT *,
-           LEAD(avg_value, 1) OVER (PARTITION BY category_code ORDER BY YEAR) lead_function
+           LEAD(avg_value, 1) OVER (PARTITION BY code ORDER BY YEAR) lead_function
     FROM czechia_price_selection
-    GROUP BY YEAR
 )
 , czechia_payroll_selection_YOY_change AS (
     SELECT *,
-           LEAD(avg_value, 1) OVER (PARTITION BY category_code ORDER BY YEAR) lead_function
+           LEAD(avg_value, 1) OVER (PARTITION BY code ORDER BY YEAR) lead_function
     FROM czechia_payroll_selection
-    GROUP BY YEAR
 )
+SELECT 
+    p.year,
+    p.avg_value AS avg_value_food_price,
+    pr.YEAR AS year,
+    p.lead_function AS lead_function_price,
+    pr.avg_value AS avg_value_food_price ,
+    ROUND((p.lead_function - p.avg_value) / p.avg_value * 100, 2) AS YOY_change_food_price,
+    pr.lead_function AS lead_function_payroll,
+    ROUND((pr.lead_function - pr.avg_value) / pr.avg_value * 100, 2) AS YOY_change_payroll
+FROM czechia_price_selection_YOY_change p
+INNER JOIN czechia_payroll_selection_YOY_change pr ON p.year = pr.YEAR
+GROUP BY pr.year;
