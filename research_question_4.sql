470890577b4ee916f4c8d7727e096d9fcfe9e097
Siobos <61201974+Siobos@users.noreply.github.com>
2024-02-11 16:08:28 +0100

    Update research_question_4.sql

--- research_question_4.sql
+++ research_question_4.sql
@@ -1,24 +1,32 @@
 /* RQ 4) Is there a year in which the year-on-year increase in food prices was significantly higher than wage growth (greater than 10%)? */
 
-WITH czechia_price_selection_YOY_change AS (
-    SELECT *,
-           LEAD(avg_value, 1) OVER (PARTITION BY code ORDER BY YEAR) lead_function
-    FROM czechia_price_selection
-)
-, czechia_payroll_selection_YOY_change AS (
-    SELECT *,
-           LEAD(avg_value, 1) OVER (PARTITION BY code ORDER BY YEAR) lead_function
+-- Creation of new combined_table --
+
+CREATE TABLE combined_table AS
+WITH payroll_data AS (
+    SELECT 
+        year,
+        AVG(avg_value) AS avg_payroll_general,
+        LEAD(AVG(avg_value), 1) OVER (PARTITION BY code ORDER BY YEAR) AS lead_function_payroll
     FROM czechia_payroll_selection
+    WHERE YEAR BETWEEN 2006 AND 2018
+    GROUP BY YEAR
+),
+price_data AS (
+    SELECT 
+        year, 
+        AVG(avg_value) AS avg_price_food_general,
+        LEAD(AVG(avg_value), 1) OVER (PARTITION BY code ORDER BY YEAR) AS lead_function_price
+    FROM czechia_price_selection
+    GROUP BY year
 )
 SELECT 
     p.year,
-    p.avg_value AS avg_value_food_price,
-    pr.YEAR AS year,
-    p.lead_function AS lead_function_price,
-    pr.avg_value AS avg_value_food_price ,
-    ROUND((p.lead_function - p.avg_value) / p.avg_value * 100, 2) AS YOY_change_food_price,
-    pr.lead_function AS lead_function_payroll,
-    ROUND((pr.lead_function - pr.avg_value) / pr.avg_value * 100, 2) AS YOY_change_payroll
-FROM czechia_price_selection_YOY_change p
-INNER JOIN czechia_payroll_selection_YOY_change pr ON p.year = pr.YEAR
-GROUP BY pr.year;
+    p.avg_payroll_general,
+    p.lead_function_payroll,
+    pr.avg_price_food_general,
+    pr.lead_function_price
+FROM payroll_data p
+JOIN price_data pr ON p.year = pr.year;
+
+-- YOY_change calculation --
